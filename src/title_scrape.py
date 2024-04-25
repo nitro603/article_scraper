486900f73463aaead76cef7044bc2b050a899796
nitro603 <tavaracristian93@gmail.com>
2024-04-24 21:07:03 -0400

    titles/topics output setup

--- src/title_scrape.py
+++ src/title_scrape.py
@@ -33,42 +33,34 @@ def make_array(input):
     return array
 
 def make_links(input):
-    
-    
     links = []
     for item in input:
-        
         item = item.lower()
-        
         topic = (find_topic(item))
         title = (find_title(item))
-        link = topic + " " + title
+        link = topic + " / " + title
         
         links.append(link)
         
-        print(links)
-    
+    print(links)
     return links
 
 def find_topic(input):
-    #the reason why I do partial string is in case there's two "-"'s in the entire string
-    partial_index = int(len(input) - 10)
+    #the reason why I create a partial string is in case there's a second "-" earlier in the string
+    partial_index = int(len(input) - 16)
     partial_string = input[partial_index:]
     
     start = partial_string.index("-") + 1
-    topic = partial_string[start:]
-    print(topic)
+    topic = partial_string[start + 1:]
     return topic
 
 def find_title(input):
     start = input.index('''"''')
     temp = input[start + 2: ]
-    
     end = temp.index('''"''')
         
-    print(start)
-    print(temp[start:end])
-    return "work in progress"
+    title = input[start + 1:end + 5]
+    return title
 
 
 url_list = ["https://www.pbs.org/newshour/science"]
@@ -77,12 +69,10 @@ prompt = '''
 '''
 
 #array = make_array(collect_info(url_list, prompt))
-array = ['''1. "WATCH: Biden marks Earth Day with new grants for solar power" - Politics''', '''2. "Why does luck play such a big role in hockey games?" - Science''', '''3. "These coral reefs suffered major damage. Watch how restoration efforts helped bring them back" - Science', '4. "Critics had accused the U.S. Interior Department and Bureau of Land Management of failing to recognize the cultural significance of the San Pedro Valley to several Native American tribes" 
-- Not Specified''', '''5. "Central U.S. faces severe thunderstorm threat and possible tornadoes" - Not Specified''', '''6. "Coral reefs across the globe once again endure mass bleaching amid warming oceans, scientists say" - Not Specified''']
-make_links(array)
 
-#print(make_link(example1))
+#array = ['''1. "WATCH: Biden marks Earth Day with new grants for solar power" - Politics''', '''2. "Why does luck play such a big role in hockey games?" - Science''', '''3. "These coral reefs suffered major damage. Watch how restoration efforts helped bring them back" - Science', '4. "Critics had accused the U.S. Interior Department and Bureau of Land Management of failing to recognize the cultural significance of the San Pedro Valley to several Native American tribes" - Not Specified''', '''5. "Central U.S. faces severe thunderstorm threat and possible tornadoes" - Not Specified''', '''6. "Coral reefs across the globe once again endure mass bleaching amid warming oceans, scientists say" - Not Specified''']
 
-#example2 = '''1. "Baltimore crews recover bodies of 2 killed in bridge collapse" - Not specified'''
-#example3 = '''9. '‘What kind of future do I have in Mississippi’? Medicaid advocates push for expansion' - Topic: Health'''
-#example4 = '''1. Title: "Baltimore crews recover bodies of 2 killed in bridge collapse" - Topic: Not specified'''
+array = ['1. "Global talks to tackle plastic pollution hit critical stage" - World', '2. "WATCH: Biden marks Earth Day with new grants for solar power" - Politics', '3. "Why does luck play such a big role in hockey games?" - Science', '4. "These coral reefs suffered major damage. Watch how restoration efforts helped bring them back" - Science', '5. "More Americans worried about climate change, but few think Biden’s climate law will help, AP-NORC poll says" - Politics']
+
+
+print(make_links(array))
