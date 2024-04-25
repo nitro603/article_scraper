e0c2825c251eff572620bcca62557d49d2c8bfa0
nitro603 <tavaracristian93@gmail.com>
2024-04-25 15:07:30 -0400

    urls almost complete

--- src/title_scrape.py
+++ src/title_scrape.py
@@ -19,8 +19,7 @@ def collect_info(url_list, query):
     index = VectorstoreIndexCreator().from_loaders(loader_list)
     ans = index.query(question=query,
                       llm=openai)
-    #print("")
-    #print(ans)
+
     return ans
 
     #outfile_name = out_name + datetime.now().strftime("%m-%d-%y-%H%M%S") + ".out"
@@ -33,16 +32,16 @@ def make_array(input):
     return array
 
 def make_links(input):
+    print(input)
     links = []
     for item in input:
         item = item.lower()
         topic = (find_topic(item))
         title = (find_title(item))
-        link = topic + " / " + title
+        link = "https://www.pbs.org/newshour/"+ topic + "/" + title
         
         links.append(link)
         
-    print(links)
     return links
 
 def find_topic(input):
@@ -55,24 +54,35 @@ def find_topic(input):
     return topic
 
 def find_title(input):
+    if "watch:" in input:
+        input = input.replace("watch:", "watch-live")
+    if "watch live:" in input:
+        input = input.replace("watch live", "")
+    if "," in input:
+        input = input.replace(",", "")
+    if "?" in input: 
+        input = input.replace("?", "")
+    if "." in input: 
+        input = input.replace(".", "")
+        
     start = input.index('''"''')
     temp = input[start + 2: ]
     end = temp.index('''"''')
         
     title = input[start + 1:end + 5]
+    title = title.replace(" ", "-")
     return title
 
 
+#url_list = ["https://www.cnbc.com/investing/"]
 url_list = ["https://www.pbs.org/newshour/science"]
-prompt = '''
-    Make a list containing the titles of the articles and their topic (example: politics, world or nation, if not specified just state not specified)?
-'''
+
+#prompt = '''  Make a list containing the titles of the articles and their topic (example: politics, world or nation, if not specified just state not specified)'''
+prompt = ''' Make a list of titles from the articles on this website '''
 
 #array = make_array(collect_info(url_list, prompt))
-
 #array = ['''1. "WATCH: Biden marks Earth Day with new grants for solar power" - Politics''', '''2. "Why does luck play such a big role in hockey games?" - Science''', '''3. "These coral reefs suffered major damage. Watch how restoration efforts helped bring them back" - Science', '4. "Critics had accused the U.S. Interior Department and Bureau of Land Management of failing to recognize the cultural significance of the San Pedro Valley to several Native American tribes" - Not Specified''', '''5. "Central U.S. faces severe thunderstorm threat and possible tornadoes" - Not Specified''', '''6. "Coral reefs across the globe once again endure mass bleaching amid warming oceans, scientists say" - Not Specified''']
-
 array = ['1. "Global talks to tackle plastic pollution hit critical stage" - World', '2. "WATCH: Biden marks Earth Day with new grants for solar power" - Politics', '3. "Why does luck play such a big role in hockey games?" - Science', '4. "These coral reefs suffered major damage. Watch how restoration efforts helped bring them back" - Science', '5. "More Americans worried about climate change, but few think Biden’s climate law will help, AP-NORC poll says" - Politics']
 
-
+#print(array)
 print(make_links(array))
